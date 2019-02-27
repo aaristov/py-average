477186983a0dfc0df0ed0102d95fc4ab39aa4468
aaristov <andrey.aristov@pasteur.fr>
2019-02-27 15:31:38 +0100

    add reading factory and csv to pandas method

--- pyaverage/iotools.py
+++ pyaverage/iotools.py
@@ -4,16 +4,23 @@ import logging
 logging.basicConfig(level='DEBUG')
 logger = logging.getLogger(__name__)
 
-def open_csv(table_path):
+def read_localization_table_from_disk(table_path: str, read_func):
+    """
+    Reading csv file into pandas.Dataframe
+    """
     try:
-        table = pd.read_csv(table_path)
+        table = read_func(table_path)
         logger.info(f'table.shape: {table.shape}')
         return table
     except IOError:
         logger.error('Wrong path')
         return False
 
-def get_xy_from(pd_table: pd.DataFrame, keys=['x [nm]', 'y [nm]']):
+def read_csv_to_pandas(path):
+    return read_localization_table_from_disk(table_path=path, read_func=pd.read_csv)
+
+
+def get_xy_from_pandas(pd_table: pd.DataFrame, keys=['x [nm]', 'y [nm]']):
     """
     Extracts xy columns from the table.
     Input: 
