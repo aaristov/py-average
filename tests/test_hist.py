3787cf898a9eca88dfec47b0881874060ebf2df4
aaristov <andrey.aristov@pasteur.fr>
2019-02-27 15:25:00 +0100

    add dimension check

--- tests/test_hist.py
+++ tests/test_hist.py
@@ -6,5 +6,6 @@ class TestGenHist2D(TestCase):
 
     def test_gen_hist2d(self):
         xy = np.random.standard_normal((50,2)) * 100
-        hist, limits = generate_hist2d(xy, px=20, shift=1, plot=True)
+        hist, limits = generate_hist2d(xy, px=20, shift=1, plot=False)
+        self.assertEqual(hist.ndim, 2)
         
